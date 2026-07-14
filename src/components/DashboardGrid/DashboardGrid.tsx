0a248269ab83476949314553966953e68ef0baf0
Sergey Akishin <morales66@mail.ru>
2026-07-14 12:30:16 +0300

    добавил мобильную верстку для ячеек

--- src/components/DashboardGrid/DashboardGrid.tsx
+++ src/components/DashboardGrid/DashboardGrid.tsx
@@ -10,8 +10,9 @@ import { DashboardDaysMobile } from "./DashboardDaysMobile";
 const timeSlots = generateTimeSlots();
 
 export const DashboardGrid = () => {
-	const currentWeekOffset = useWeekStore((state) => state.currentWeekOffset);
-	const currentDayDate = useWeekStore((state) => state.currentDate)
+  const currentWeekOffset = useWeekStore((state) => state.currentWeekOffset)
+  const currentDayDate = useWeekStore((state) => state.currentDate);
+  const activeDate = useWeekStore((state) => state.activeDate);
   const getWeekDays = useWeekStore((state) => state.getWeekDays);
   const days = useMemo(() => getWeekDays(), [currentWeekOffset, getWeekDays]);
 
@@ -20,10 +21,11 @@ export const DashboardGrid = () => {
     [styles.cell]: true,
   });
 
-	return (
+  return (
     <>
-      <DashboardDays days={days} currentDayDate={currentDayDate}/>
-			<DashboardDaysMobile days={days} currentDayDate={currentDayDate}/>
+      {console.log(days)}
+      <DashboardDays days={days} currentDayDate={currentDayDate} />
+      <DashboardDaysMobile days={days} currentDayDate={currentDayDate} />
       <div className={styles.grid}>
         {timeSlots.map((timeSlot) => {
           return (
@@ -31,15 +33,39 @@ export const DashboardGrid = () => {
               <div className={timeCell} key={`time-${timeSlot}`}>
                 {timeSlot}
               </div>
-              {days.map((days, dayIndex) => {
+              {days.map((day, dayIndex) => {
                 return (
-									<DashboardCell 
-										key={`${dayIndex}-${timeSlot}`}
-										dayIndex={dayIndex}
-										timeSlot={timeSlot}
-										date={days.dateObject}
-									/>
+                  <DashboardCell
+                    key={`${dayIndex}-${timeSlot}`}
+                    dayIndex={dayIndex}
+                    timeSlot={timeSlot}
+                    date={day.dateObject}
+                  />
                 );
+              })}
+            </React.Fragment>
+          );
+        })}
+      </div>
+      <div className={styles.gridMobile}>
+        {timeSlots.map((timeSlot) => {
+          return (
+            <React.Fragment key={`row-${timeSlot}`}>
+              <div className={timeCell} key={`time-${timeSlot}`}>
+                {timeSlot}
+              </div>
+              {days.map((day, dayIndex) => {
+                if (day.dateObject === activeDate) {
+                  return (
+                    <DashboardCell
+                      key={`${dayIndex}-${timeSlot}`}
+                      dayIndex={dayIndex}
+                      timeSlot={timeSlot}
+                      date={day.dateObject}
+                    />
+                  );
+                }
+                return null
               })}
             </React.Fragment>
           );
